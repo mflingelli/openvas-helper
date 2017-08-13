42757654d475d6aeccb131ee939eaec6eb6a1c40
Markus Flingelli <markus.flingelli@gmx.de>
2017-08-13 09:16:27 +0200

    Unused import statement removed.

--- src/main/java/de/flingelli/security/openvas/reports/data/Count.java
+++ src/main/java/de/flingelli/security/openvas/reports/data/Count.java
@@ -2,7 +2,6 @@ package de.flingelli.security.openvas.reports.data;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlValue;
 
 @XmlAccessorType(XmlAccessType.FIELD)
